5a7709e6b9aa5804d7768b0488d78318c971fb2f
Kevin Colten <kevincolten@gmail.com>
2015-06-14 18:21:18 -0500

    blog showing up, added resume json

--- Posts.js
+++
@@ -1,9 +0,0 @@
-Backbone.PostCollection = Backbone.Collection.extend({
-	url: "https://raw.githubusercontent.com/mistakevin/mistakevin.github.io/master/posts.json"
-})
-var posts = new Backbone.PostCollection();
-posts.fetch({
-	success: function(collection) {
-		console.log(collection);
-	}
-});
